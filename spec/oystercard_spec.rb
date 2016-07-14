8d7903217a93978966f187172fd2878f3a544e99
Nomi Vos <nvoswork@gmail.com>
2016-07-14 14:09:46 -0500

    05_top_up.md tessed passed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,9 +4,14 @@ describe Oystercard do
   subject(:oystercard) { described_class.new }
 
   context 'regarding card' do
-    #it { is_expected.to respond_to :balance }
     it 'checks to see if the balance is zero' do
       expect(oystercard.balance).to eq 0
+    end
+
+    describe '#top_up' do
+      it 'checks to see if the card is topped up' do
+        expect{ oystercard.top_up 1}.to change{ subject.balance }.by 1
+      end
     end
   end
 
