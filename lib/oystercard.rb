330f33f2ac1312d056f215bc7135cc09eed16f9b
Nomi Vos <nvoswork@gmail.com>
2016-07-14 14:32:27 -0500

    06_maximum_balance.md tests passed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,15 @@
 class Oystercard
   attr_reader :balance
 
+  MAX_BALANCE = 90
+  MIN_FARE = 1
+
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail 'maximum balance exceeded' if amount + balance > MAX_BALANCE
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,6 +3,9 @@ require 'oystercard'
 describe Oystercard do
   subject(:oystercard) { described_class.new }
 
+  maximum_balance = Oystercard::MAX_BALANCE
+  min_fare = Oystercard::MIN_FARE
+
   context 'regarding card' do
     it 'checks to see if the balance is zero' do
       expect(oystercard.balance).to eq 0
@@ -10,7 +13,11 @@ describe Oystercard do
 
     describe '#top_up' do
       it 'checks to see if the card is topped up' do
-        expect{ oystercard.top_up 1}.to change{ subject.balance }.by 1
+        expect{ oystercard.top_up(1)}.to change{ subject.balance }.by 1
+      end
+      it 'raises an error if top_up exceeds maximum balance' do
+        oystercard.top_up(maximum_balance)
+        expect{ oystercard.top_up(min_fare)}.to raise_error 'maximum balance exceeded'
       end
     end
   end
